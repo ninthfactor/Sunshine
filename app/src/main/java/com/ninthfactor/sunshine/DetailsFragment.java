64b3707ae8ade32d59e67c4d6cdf975a027f843f
Ninth Factor <ninthfactor@gmail.com>
2015-08-07 21:41:57 -0400

    updated shared intent

--- app/src/main/java/com/ninthfactor/sunshine/DetailsFragment.java
+++ app/src/main/java/com/ninthfactor/sunshine/DetailsFragment.java
@@ -1,5 +1,4 @@
 package com.ninthfactor.sunshine;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -26,8 +25,8 @@ public class DetailsFragment extends Fragment {
   //  private ShareActionProvider mShareActionProvider;
     private static final String LOG_TAG = DetailsFragment.class.getSimpleName();
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
-    private String message;
-    private ShareActionProvider shareActionProvider;
+    private String mForecastStr;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,10 +38,26 @@ public class DetailsFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.details_fragment, menu);
-        MenuItem menuItem = menu.findItem(R.id.menu_detail_item_share);
-        shareActionProvider = new ShareActionProvider(this.getActivity());
-        MenuItemCompat.setActionProvider(menuItem, shareActionProvider);
+
+        // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null ) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+
     }
+
+    /*
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -50,33 +65,23 @@ public class DetailsFragment extends Fragment {
         //buttons as long as we specify parent activity in Android Manifest file.
 
         int id = item.getItemId();
-        if (id == R.id.menu_detail_item_share) {
+        if (id == R.id.action_share) {
             //TODO: Code for Share Action intent.
 
-            //mShareActionProvider = (ShareActionProvider) item.getActionProvider();
-        //    ShareActionProvider mShareActionProvider =
-         //           (ShareActionProvider) MenuItemCompat.getActionProvider(item);
-
-            // Attach an intent to this ShareActionProvider.  You can update this at any time,
-            // like when the user selects a new piece of data they might like to share.
-
-            if (ShareActionProvider != null) {
-                ShareActionProvider.setShareIntent(createShareForecastIntent());
-            } else {
-                Log.d(LOG_TAG, "Share Action Provider is null?");
-            }
         }
 
         return super.onOptionsItemSelected(item);
 
     }
 
+    */
+
     private Intent createShareForecastIntent() {
                     Intent shareIntent = new Intent(Intent.ACTION_SEND);
                     shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
                     shareIntent.setType("text/plain");
                     shareIntent.putExtra(Intent.EXTRA_TEXT,
-                                      message + FORECAST_SHARE_HASHTAG);
+                                      mForecastStr + FORECAST_SHARE_HASHTAG);
                     return shareIntent;
                 }
 
@@ -87,12 +92,12 @@ public class DetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         Intent intent = getActivity().getIntent(); // Activity gets the intent. So we need first
                                 // get the activity and then intent in it.
-        message = intent.getStringExtra(ForecastFragment.EXTRA_MESSAGE);
+        mForecastStr = intent.getStringExtra(ForecastFragment.EXTRA_MESSAGE);
 
         //  Get root view and then find the textview to update message from intent.
         View rootView = inflater.inflate(R.layout.fragment_details, container, false);
         TextView detailWeather = (TextView) rootView.findViewById(R.id.detailWeather);
-        detailWeather.setText(message);
+        detailWeather.setText(mForecastStr);
         
         return rootView ;
     }
